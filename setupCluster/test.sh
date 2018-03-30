49f7277c7e732deca0bff82abc1c58c9692e79f1
Kevin Wu <kevin.wu@magicwindow.cn>
2018-03-30 10:43:11 +0800

    init comit

---
+++ setupCluster/test.sh
@@ -0,0 +1,7 @@
+../bin/configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
+
+
+../bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID mychannel -asOrg Org1MSP
+
+
+../bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID mychannel -asOrg Org2MSP
