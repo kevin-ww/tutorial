5b2322835178e343b331f0243ec9b37f08fff55e
Kevin Wu <kevin.wu@magicwindow.cn>
2018-04-03 10:16:58 +0800

    not using ssl

--- setupCluster/test.sh
+++ setupCluster/test.sh
@@ -31,3 +31,9 @@ peer chaincode install -n mycc -v 1.0 -p github.com/hyperledger/fabric/peer/chan
 peer chaincode instantiate -o orderer0.orgorderer1:7050 -C mychannel -n mycc -v 1.0 -c '{"Args":["init","a","100","b","200"]}'
 
 peer chaincode invoke -o orderer0.orgorderer1:7050  -C $CHANNEL_NAME -n mycc -c '{"Args":["invoke","a","b","10"]}'
+
+
+
+composer card create -p /tmp/composer/org1/byfn-network-org1.json -u PeerAdmin -c /tmp/composer/org1/Admin@org1-cert.pem -k /tmp/composer/org1/*_sk -r PeerAdmin -r ChannelAdmin -f PeerAdmin@byfn-network-org1.card
+
+composer card create -p /tmp/composer/org2/byfn-network-org2.json -u PeerAdmin -c /tmp/composer/org2/Admin@org2-cert.pem -k /tmp/composer/org2/*_sk -r PeerAdmin -r ChannelAdmin -f PeerAdmin@byfn-network-org2.card
